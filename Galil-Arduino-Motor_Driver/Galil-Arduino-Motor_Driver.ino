0d58c2641ff51f6a065ea17dcd6008419d769302
Jacob Lawrence <lawrence.jacob@outlook.com>
2021-09-21 13:26:18 -0600

    UPDATED ARDUINO

--- Galil-Arduino-Motor_Driver/Galil-Arduino-Motor_Driver.ino
+++ Galil-Arduino-Motor_Driver/Galil-Arduino-Motor_Driver.ino
@@ -10,24 +10,46 @@ void loop() {
 */
 int value = 0;
 int inPin = 13;
+int pwmPin = 6;
+int switchpin1 = 4;
+int switchpin2 = 5;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(A5, INPUT);
+  pinMode(inPin, INPUT);
+  pinMode(pwmPin, OUTPUT);
+  pinMode(switchpin1, OUTPUT);
+  pinMode(switchpin2, OUTPUT);
+  digitalWrite(switchpin1, HIGH);
+  digitalWrite(switchpin2, LOW);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  value = digitalRead(A5);   // read the input pin
+  
+  value = digitalRead(inPin);   // read the input pin
   digitalWrite(LED_BUILTIN, value);
   //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  //delay(1000);                       // wait for a second
   //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   //delay(1000);                       // wait for a second
-  
-  //digitalWrite(LED_BUILTIN, LOW); 
-  //delay(500);
+ 
+ if(value == HIGH) {
+    analogWrite(pwmPin, 255);
+    //digitalWrite(LED_BUILTIN, HIGH);
+    //delay(1000); 
+ }
+ else {
+    analogWrite(pwmPin, 0);
+    //digitalWrite(LED_BUILTIN, LOW);
+    //delay(1000); 
+ }
+
+ //analogWrite(pwmPin, 255);
+ //digitalWrite(LED_BUILTIN, LOW); 
+ //delay(1000);
+ 
   
 }
