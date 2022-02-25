382632d497635c9b2b8b13ad505d1432c2f14523
jacobel27 <lawrence.jacob@outlook.com>
2022-02-24 18:14:55 -0700

    High Speed Logic Start

--- src/widgets/highspeedlinewidget.cpp
+++ src/widgets/highspeedlinewidget.cpp
@@ -10,11 +10,13 @@ HighSpeedLineWidget::HighSpeedLineWidget(QWidget *parent) : PrinterWidget(parent
 {
     ui->setupUi(this);
     setAccessibleName("High Speed Line Widget");
+    print = new HighSpeedLineCommandGenerator{};
     setup();
 }
 
 HighSpeedLineWidget::~HighSpeedLineWidget()
 {
+    delete print;
     delete ui;
 }
 
@@ -25,7 +27,77 @@ void HighSpeedLineWidget::allow_widget_input(bool allowed)
 
 void HighSpeedLineWidget::setup()
 {
-    ui->SVGViewer->setup(2, 0.5);
+    // Connect all Spin Boxes to update print settings when finished editing
+    QList<QAbstractSpinBox *> printSettingWidgets = this->findChildren<QAbstractSpinBox *> ();
+    for(int i{0}; i < printSettingWidgets.count(); ++i)
+    {
+       connect(printSettingWidgets[i], &QAbstractSpinBox::editingFinished, this, &HighSpeedLineWidget::update_print_settings);
+    }
+
+    // Connect all QComboBoxes to update print settings when index is changed
+    QList<QComboBox *> printSettingWidgets2 = this->findChildren<QComboBox *> ();
+    for(int i{0}; i < printSettingWidgets2.count(); ++i)
+    {
+       connect(printSettingWidgets2[i], qOverload<int>(&QComboBox::currentIndexChanged), this, &HighSpeedLineWidget::update_print_settings);
+    }
+
+    update_print_settings();
+}
+
+void HighSpeedLineWidget::update_print_settings()
+{
+    // check for if other things need to be updated
+    bool mustUpdatePreviewWindow{false};
+    Axis currentViewAxis = print->viewAxis;
+    Axis newViewAxis = ui->viewAxisComboBox->currentIndex() == 0 ? Axis::X : Axis::Y;
+
+    // check to see if build box stats changed and update the preview window if needed
+    if (currentViewAxis != newViewAxis) mustUpdatePreviewWindow = true;
+    else if (print->buildBox.length != ui->buildBoxLengthSpinBox->value()) mustUpdatePreviewWindow = true;
+    else if (print->buildBox.thickness != ui->buildBoxThicknessSpinBox->value()) mustUpdatePreviewWindow = true;
+
+    // update print parameters
+    print->numLines = ui->numLinesSpinBox->value();
+    print->lineSpacing_um = ui->lineSpacingSpinBox->value();
+    print->lineLength_mm = ui->lineLengthSpinBox->value();
+    print->dropletSpacing_um = ui->dropletSpacingSpinBox->value();
+    print->jettingFrequency_Hz = ui->jettingFrequencySpinBox->value();
+    print->acceleration_mm_per_s = ui->printAccelerationSpinBox->value();
+
+    // update build box parameters
+    print->buildBox.centerX = ui->buildBoxCenterXSpinBox->value();
+    print->buildBox.centerY = ui->buildBoxCenterYSpinBox->value();
+    print->buildBox.thickness = ui->buildBoxThicknessSpinBox->value();
+    print->buildBox.length = ui->buildBoxLengthSpinBox->value();
+
+    // update observation settings - first item in box should be x, second is y
+    print->printAxis = ui->printAxisComboBox->currentIndex() == 0 ? Axis::X : Axis::Y;
+    print->viewAxis = ui->viewAxisComboBox->currentIndex() == 0 ? Axis::X : Axis::Y;
+    print->triggerOffset_ms =ui->triggerOffsetSpinBox->value();
+
+    // disable the line spacing spin box if there is only one line being printed
+    if (print->numLines == 1) ui->lineSpacingSpinBox->setEnabled(false);
+    else ui->lineSpacingSpinBox->setEnabled(true);
+
+    if (mustUpdatePreviewWindow)
+    {
+        if (print->viewAxis == Axis::Y)
+        {
+            ui->SVGViewer->setup(print->buildBox.length, print->buildBox.thickness);
+            qDebug() << print->buildBox.length;
+            qDebug() << print->buildBox.thickness;
+        }
+        else
+        {
+            ui->SVGViewer->setup(print->buildBox.thickness, print->buildBox.length);
+        }
+    }
+}
+
+// this is just here for the QComboBoxes to be able to update print settings
+void HighSpeedLineWidget::update_print_axes(int index)
+{
+    update_print_settings();
 }
 
 void HighSpeedLineWidget::reset_preview_zoom()
@@ -34,6 +106,18 @@ void HighSpeedLineWidget::reset_preview_zoom()
 }
 
 void HighSpeedLineWidget::print_line()
+{
+    std::vector<std::stringstream> lineCommands;
+
+    for (auto &printLine : lineCommands)
+    {
+        emit execute_command(printLine);
+        // wait here for user input
+        // break if the user wants to stop
+    }
+}
+
+std::vector<std::string> HighSpeedLineCommandGenerator::print_commands_for_lines()
 {
     std::stringstream s;
 
@@ -77,6 +161,8 @@ void HighSpeedLineWidget::print_line()
 
     s << CMD::at_time_samples(TriggerOffsetTime); // WHAT DO I PUT HERE?
 
-    emit execute_command(s);
+    std::vector<std::string> returnVec;
+    returnVec.push_back(s.str());
+    return returnVec;
 }
 
--- src/widgets/highspeedlinewidget.h
+++ src/widgets/highspeedlinewidget.h
@@ -5,6 +5,41 @@
 #include <QPen>
 
 #include "printerwidget.h"
+#include "printer.h"
+#include <sstream>
+
+struct SmallBuildBox
+{
+    double centerX{};
+    double centerY{};
+    double thickness{};
+    double length{};
+};
+
+class HighSpeedLineCommandGenerator
+{
+public:
+    // stringstreams are not copyable... is there a better way to handle this?
+    // maybe I can return a reference to an interal vector of stringstreams?
+    std::vector<std::string> print_commands_for_lines();
+
+    int numLines{};
+    int lineSpacing_um{};
+    double lineLength_mm{};
+    int dropletSpacing_um{};
+    int jettingFrequency_Hz{};
+    int acceleration_mm_per_s{};
+
+    SmallBuildBox buildBox{};
+
+    Axis printAxis{};
+    Axis viewAxis{};
+
+    int triggerOffset_ms{};
+
+private:
+    std::stringstream s_;
+};
 
 namespace Ui
 {
@@ -24,9 +59,12 @@ public:
 private slots:
     void print_line();
     void setup();
+    void update_print_settings();
+    void update_print_axes(int index);
 
 private:
     Ui::HighSpeedLineWidget *ui;
+    HighSpeedLineCommandGenerator *print{nullptr};
 };
 
 #endif // HIGHSPEEDLINEWIDGET_H
