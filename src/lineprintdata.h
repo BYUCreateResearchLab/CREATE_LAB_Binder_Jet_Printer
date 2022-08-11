8de775ce525ce2ba508d7dd9acbd04f842a878fc
jacobel27 <lawrence.jacob@outlook.com>
2022-08-11 12:20:57 -0600

    Jetting Freq Unlock
    Allow decimal jetting frequencies and allow the user to go up to 2000 Hz jetting frequency.

--- src/lineprintdata.h
+++ src/lineprintdata.h
@@ -87,15 +87,16 @@ class LineSet
 {
 public:
     LineSet();
-                              // "Display Name", data type, default value, min, max
-    TableData numLines          {"Number of Lines", type::int_type, 6, 1, 5000};
-    TableData lineSpacing       {"Line Spacing\n(mm)", type::float_type, 12, 0.005f, 50};
-    TableData lineLength        {"Line Length\n(mm)", type::float_type, 15, 0.01f, 100};
-    TableData dropletSpacing    {"Droplet Spacing\n(µm)", type::int_type, 15, 1, 300};
-    TableData jettingFreq       {"Jetting Frequency\n(Hz)", type::int_type, 1000, 20, 10000};
-    TableData printVelocity     {"Printing Velocity\n(mm/s)", type::float_type, 15, 0.1f, 10000};
-    TableData printAcceleration {"Print Acceleration\n(mm/s2)", type::float_type, 800, 100.0, 2000.0};
-    int size = 7; // Number of columns in dataset
+                              // "Display Name",                data type,     default, min,    max
+    TableData numLines          {"Number of Lines",             type::int_type,   6,    1,      5000};
+    TableData lineSpacing       {"Line Spacing\n(mm)",          type::float_type, 12,   0.005f, 50};
+    TableData lineLength        {"Line Length\n(mm)",           type::float_type, 15,   0.01f,  100};
+    TableData dropletSpacing    {"Droplet Spacing\n(µm)",       type::int_type,   15,   1,      300};
+    TableData jettingFreq       {"Jetting Frequency\n(Hz)",     type::float_type, 2000, 20,     10000};
+    TableData printVelocity     {"Printing Velocity\n(mm/s)",   type::float_type, 15,   0.1f,   10000};
+    TableData printAcceleration {"Print Acceleration\n(mm/s2)", type::float_type, 800,  100.0,  2000.0};
+    int size = 7; // Number of columns in dataset (make sure this is the same as the number of objects above)
+    // also, make sure to add them to the switch statement below
 
     TableData& operator[](int i)
     {
