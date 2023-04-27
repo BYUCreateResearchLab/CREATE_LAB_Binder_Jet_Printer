e6fc23178eb9279cf8d9d80bcdcd31dc3f840413
jacobel27 <lawrence.jacob@outlook.com>
2023-04-27 17:23:07 -0600

    PrintThread

--- src/widgets/printerwidget.cpp
+++ src/widgets/printerwidget.cpp
@@ -1,9 +1,11 @@
 #include "printerwidget.h"
+#include "dmc4080.h"
 
 PrinterWidget::PrinterWidget(Printer *printer_, QWidget *parent) :
     QWidget(parent),
     mPrinter(printer_)
 {
+    mPrintThread = mPrinter->mcu->printerThread;
 }
 
 PrinterWidget::~PrinterWidget()
